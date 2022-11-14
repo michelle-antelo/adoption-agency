0fa97c7715917f575cc915f34cc9f5233c0bdeec
Michelle Antelo <michelle@Michelles-MacBook-Pro.local>
2022-11-14 13:50:56 -0500

    broken :(

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask, request, render_template, redirect, flash
-from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, Pet
+from flask import Flask, url_for, render_template, redirect, flash, jsonify, request
+# from models import db, connect_db, Pet
 from forms import AddPetForm
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://michelle:1003866Ma@localhost/pet_database'
+app.config['SECRET_KEY'] = "a secret"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///pet_database"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = "a secret!"
-debug = DebugToolbarExtension(app)
 
-connect_db(app)
+print("Before")
+# connect_db(app)
+print("After: ")
 
 @app.route("/")
 def render_homepage():
@@ -30,3 +31,5 @@ def add_pet():
 
     else:
         return render_template("add-pet-form.html", form=form)
+
+    
--- models.py
+++ models.py
@@ -1,24 +1,31 @@
 from flask_sqlalchemy import SQLAlchemy
 
+DEFAULT_IMAGE = "https://64.media.tumblr.com/58ad30500e096781e56e5f31e50c561c/0767393739a2484f-05/s540x810/2bfbc14411783260ef0d80c4cafd99a55a12aca1.pnj"
+
 db = SQLAlchemy()
 
-def connect_db(app):
-    """Connect to database."""
-
-    db.app = app
-    db.init_app(app)
 
 class Pet(db.Model):
-    """Pet."""
+    """Pet Database"""
 
     __tablename__ = "pets"
 
-    id = db.Column(db.Integer,
-                   primary_key=True,
-                   autoincrement=True)
-    name = db.Column(db.String(50), nullable=False)
-    # species = db.Column(db.String(30), nullable=False)
-    # photo_url = db.Column(db.String, nullable=True)
-    # age = db.Column(db.Integer, nullable=True)
-    # notes = db.Column(db.String, nullable = True)
-    # available = db.Column(db.Boolean, nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    species = db.Column(db.Text, nullable=False)
+    photo_url = db.Column(db.Text)
+    age = db.Column(db.Integer)
+    notes = db.Column(db.Text)
+    available = db.Column(db.Boolean, nullable=False, default=True)
+
+    def image_url(self):
+        """Submitted image or default image"""
+
+        return self.photo_url or DEFAULT_IMAGE
+
+
+def connect_db(app):
+    """Connect to database"""
+
+    db.app = app
+    db.init_app(app)
